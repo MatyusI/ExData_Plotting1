e3310f8c6c954e65b57cf31d9f329ec9f9bb41cf
Iren Matyus <MATYUSI@OTPBANK.HU>
2017-04-24 16:00:40 +0200

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -1,8 +1,8 @@
 dataFile <- "C:/Users/MatyusI/Documents/R_kodok/C4_Exploratory Data Analisys/household_power_consumption.txt"
- data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
- #str(subSetData)
+#str(subSetData)
 globalActivePower <- as.numeric(subSetData$Global_active_power)
-png("plot1.png", width=480, height=480)
+png("C:/Users/MatyusI/Documents/R_kodok/C4_Exploratory Data Analisys/plot1.png", width=480, height=480)
 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
